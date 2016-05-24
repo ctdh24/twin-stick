01cac5d4fc01048b2fe0b777a1375b6944e3943e
ctdh24 <ctdh24@gmail.com>
2016-05-23 21:50:24 -0700

    init commit

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+    public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += (Vector3.right * Input.GetAxis(horizontalAxis) + Vector3.forward * Input.GetAxis(verticalAxis))*speed*Time.deltaTime;
+
+    }
+}
---
+++ Assets/scripts/PlayerShoot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+    public GameObject bullet;
+    public float bulletSpeed = 5.0f;
+
+    public float shootDelay = 0.1f;
+
+    private bool canShoot = true;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 shootDirection = Vector3.right * Input.GetAxis(horizontalAxis) + Vector3.forward * Input.GetAxis(verticalAxis);
+        if (canShoot && shootDirection.sqrMagnitude > 0.0f) {
+            transform.rotation = Quaternion.LookRotation(shootDirection, Vector3.up);
+            Instantiate(bullet, transform.position, transform.rotation);
+            canShoot = false;
+            Invoke("resetShoot", shootDelay);
+        }
+	}
+    void resetShoot()
+    {
+        if(!canShoot)
+            canShoot = true;
+    }
+}
